54247edb734d8cb763ef8a98652a58488d1eb561
Preethesh-07 <poojarypreethesh10@gmail.com>
2025-09-27 17:01:17 +0530

    Test passed

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from streamlit-deployment-preethesh!")
+
+
+if __name__ == "__main__":
+    main()
--- streamlit_chatbot.py
+++ streamlit_chatbot.py
@@ -3,5 +3,5 @@ import streamlit as st
 def main():
     print("Hello world")
 
-def __name__=="__main__":
+if __name__ == "__main__":
     main()
